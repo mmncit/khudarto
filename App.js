af33f0a5ed33a167e2ffebca6f55214342c076b0
Mamun <mmncit@gmail.com>
2023-03-01 02:08:17 -0700

    add prettier and eslint

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {  Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text className="text-red-500">Home Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View className="mt-8 px-2">
-      <Text className="text-red-500">
-        Open up App.js to start working on your app!! Right?
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
