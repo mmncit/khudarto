46582b422c7080b258c0720a8db80d48df8ce53e
Mamun <mmncit@gmail.com>
2023-03-04 11:28:03 -0700

    added a scroll view

---
+++ components/Categories.js
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Categories = () => {
+  return <Text>Categories Hello</Text>;
+};
+
+export default Categories;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native';
 import * as React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
@@ -7,6 +7,7 @@ import {
   UserIcon,
   MagnifyingGlassIcon,
 } from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -20,13 +21,13 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-white pt-5">
       {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image
           source={require('../assets/logo.avif')}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
         <View className="flex-1">
-          <Text className="font-bold text-gray-400">Deliver now!</Text>
+          <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
           <Text className="font-bold text-xl">
             Current Location
             <ChevronDownIcon size={20} color="#00CCBB" />
@@ -35,13 +36,22 @@ const HomeScreen = () => {
         <UserIcon size={35} color="#00CCBB" />
       </View>
       {/* Search */}
-      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
         <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
           <MagnifyingGlassIcon color="gray" size={20} />
           <TextInput placeholder="Restaurants and cuisines" keyboardType="default" />
         </View>
         <AdjustmentsVerticalIcon color="#00CCBB" />
       </View>
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <Categories />
+      </ScrollView>
     </SafeAreaView>
   );
 };
